9ba4af8122b34de5c08a6af02d03a9f8e79d4f5b
YANGYIFU <km576843117@hotmail.com>
2016-03-14 12:58:03 +0800

    add solution 11

---
+++ LeetcodeSolutions/LeetcodeSolutions/11.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int res = 0;
+
+        int left = 0, right = height.Length - 1;
+        while (left != right)
+        {
+            int tmp = (right - left) * (height[left] > height[right] ? height[right] : height[left]);
+            if (tmp > res)
+                res = tmp;
+            if (height[right] < height[left])
+                right--;
+            else
+                left++;
+        }
+        return res;
+    }
+}
