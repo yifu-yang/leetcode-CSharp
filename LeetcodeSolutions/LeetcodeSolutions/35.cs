5913f0967fe8775c769c9f51016b9646117c5c25
YANGYIFU <km576843117@hotmail.com>
2016-03-18 11:08:25 +0800

    add solution 35

---
+++ LeetcodeSolutions/LeetcodeSolutions/35.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int length = nums.Length;
+        int start = 0, end = length - 1, mid = (start + end) / 2;
+
+        if (target < nums[start])
+            return start;
+        if (target > nums[end])
+            return length;
+        while (start <= end)
+        {
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            if (target > nums[mid])
+            {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+            mid = (start + end) / 2;
+        }
+
+        if (nums[mid] < target)
+            return mid + 1;
+        else
+            return mid;
+    }
+}
