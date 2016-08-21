9949968052d71aff94d3172716930d730c25b224
YANGYIFU <km576843117@hotmail.com>
2016-08-21 20:42:16 +0800

    test for Aug 21 ,2016

---
+++ LeetcodeSolutions/LeetcodeSolutions/387.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int FirstUniqChar(string s)
+    {
+        Dictionary<string, int> charCount = new Dictionary<string, int>();
+        HashSet<string> notUniq = new HashSet<string>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (notUniq.Contains(s[i].ToString()))
+                continue;
+            if (!charCount.ContainsKey(s[i].ToString()))
+            {
+                charCount.Add(s[i].ToString(), i);
+            }
+            else
+            {
+                charCount.Remove(s[i].ToString());
+                notUniq.Add(s[i].ToString());
+            }
+        }
+        if (charCount.Count != 0)
+            return charCount.OrderBy(x => x.Value).First().Value;
+        else
+            return -1;
+    }
+}
