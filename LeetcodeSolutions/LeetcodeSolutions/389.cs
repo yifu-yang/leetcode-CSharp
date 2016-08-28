c3ae88cc31e0bd61c8c4f65c6f5d710219c6577e
YANGYIFU <km576843117@hotmail.com>
2016-08-28 11:17:41 +0800

    Aug 28 test

---
+++ LeetcodeSolutions/LeetcodeSolutions/389.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public char FindTheDifference(string s, string t)
+    {
+        var source = new Dictionary<char, int>();
+        foreach (char c in t)
+        {
+            if (source.ContainsKey(c))
+            {
+                source[c]++;
+            }
+            else
+            {
+                source.Add(c, 1);
+            }
+        }
+        foreach (char c in s)
+        {
+            source[c]--;
+
+        }
+        return source.Where(x => x.Value > 0).Select(x => x.Key).First();
+    }
+}
