79cfe1410a062356d38d5f561e3b09939f92056b
YANGYIFU <km576843117@hotmail.com>
2016-09-04 22:42:12 +0800

    Sep 4th contest

---
+++ LeetcodeSolutions/LeetcodeSolutions/392.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        int tStep = 0, sStep = 0;
+
+        if (s.Length > t.Length)
+            return false;
+
+        if (s.Equals(t))
+            return true;
+
+        for (tStep = 0; tStep < t.Length; tStep++)
+        {
+
+            if (s.Length > sStep && t[tStep].Equals(s[sStep]))
+            {
+                sStep++;
+            }
+
+            if (sStep == s.Length - 1 || s.Length == 0)
+                return true;
+        }
+        return false;
+    }
+}
