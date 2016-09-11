0ebe0e1ffbafd9be1b99ece2fac48d9d3bb656f5
YANGYIFU <km576843117@hotmail.com>
2016-09-11 11:37:55 +0800

    sept 11th contest

---
+++ LeetcodeSolutions/LeetcodeSolutions/396.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int MaxRotateFunction(int[] A)
+    {
+        if (A.Length == 0)
+            return 0;
+        var result = int.MinValue;
+        var n = A.Length;
+        for (int i = 0; i < n; i++)
+        {
+            var tmp = 0;
+            int j = n - i;
+            int p = 0;
+            for (; j < n; j++, p++)
+            {
+                tmp += A[j] * p;
+            }
+            for (int k = 0; k < n - i; k++)
+            {
+                tmp += A[k] * (k + i);
+            }
+            if (tmp > result)
+                result = tmp;
+        }
+        return result;
+    }
+}
