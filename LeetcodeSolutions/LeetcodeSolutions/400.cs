c7b8788a2ba190756156fc3194ce20f393e47520
YANGYIFU <km576843117@hotmail.com>
2016-09-18 11:18:34 +0800

    sept 18 2016 contest

---
+++ LeetcodeSolutions/LeetcodeSolutions/400.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public int FindNthDigit(int n)
+    {
+        double tmp = 0;
+        double site = 0;
+        if (n < 10)
+            return n;
+        for (int i = 1; ; i++)
+        {
+            if (n > tmp + 9 * Math.Pow(10, i - 1) * i)
+            {
+                tmp += 9 * Math.Pow(10, i - 1) * i;
+                site += 9 * Math.Pow(10, i - 1);
+                continue;
+            }
+            else
+            {
+                var k = n - tmp;
+                var number = (int)k / i + site + ((int)k % i > 0 ? 1 : 0);
+                return (number).ToString()[(int)(k - 1) % (i)] - '0';
+            }
+        }
+    }
+}
